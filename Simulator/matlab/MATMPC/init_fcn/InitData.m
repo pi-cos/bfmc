c4ac1cf69c0c532063de46e976a2ccadc0bf1500
Enrico Picotti <enrico.picotti@gmail.com>
2022-10-13 12:15:22 +0200

    implemented kinematic vehicle nmpc (on itself)

---
+++ Simulator/matlab/MATMPC/init_fcn/InitData.m
@@ -0,0 +1,104 @@
+%% Initialize Data
+function [input] = InitData(settings)
+
+    nx = settings.nx;       % No. of differential states
+    nu = settings.nu;       % No. of controls
+    nz = settings.nz;       % No. of algebraic states
+    ny = settings.ny;        % No. of outputs (references)    
+    nyN= settings.nyN;       % No. of outputs at terminal stage 
+    np = settings.np;        % No. of parameters (on-line data)
+    nc = settings.nc;        % No. of constraints
+    ncN = settings.ncN;      % No. of constraints at terminal stage
+    N  = settings.N;         % No. of shooting points
+    nbx = settings.nbx;      % No. of state bounds
+    nbu = settings.nbu;      % No. of control bounds
+    nbu_idx = settings.nbu_idx;  % Index of control bounds
+
+    switch settings.model
+                      
+        case 'InvertedPendulum'
+            input.x0 = [0;pi;0;0];    
+            input.u0 = zeros(nu,1); 
+            input.z0 = zeros(nz,1);
+            para0 = 0;  
+
+            Q=repmat([10 10 0.1 0.1 0.01]',1,N);
+            QN=[10 10 0.1 0.1]';
+
+            % upper and lower bounds for states (=nbx)
+            lb_x = -2;
+            ub_x = 2;
+
+            % upper and lower bounds for controls (=nbu)           
+            lb_u = -20;
+            ub_u = 20;
+                       
+            % upper and lower bounds for general constraints (=nc)
+            lb_g = [];
+            ub_g = [];            
+            lb_gN = [];
+            ub_gN = [];
+
+        case 'KinematicVehicle'
+            input.x0 = zeros(nx,1);
+            input.u0 = zeros(nu,1);
+            input.z0 = zeros(nz,1);
+            para0 = [0;0.4;0.5];
+
+            Q=repmat([1 1 0.1]',1,N);
+            QN=[1]';
+
+            % upper and lower bounds for states (=nbx)
+            lb_x = [];
+            ub_x = [];
+
+            % upper and lower bounds for controls (=nbu)
+            lb_u = [-1;-pi/6];
+            ub_u = [+1;+pi/6];
+
+            % upper and lower bounds for general constraints (=nc)
+            lb_g = [];
+            ub_g = [];
+            lb_gN = [];
+            ub_gN = [];
+                                                            
+    end
+
+    % prepare the data
+    
+    input.lb = repmat(lb_g,N,1);
+    input.ub = repmat(ub_g,N,1);
+    input.lb = [input.lb;lb_gN];
+    input.ub = [input.ub;ub_gN];
+            
+    lbu = -inf(nu,1);
+    ubu = inf(nu,1);
+    for i=1:nbu
+        lbu(nbu_idx(i)) = lb_u(i);
+        ubu(nbu_idx(i)) = ub_u(i);
+    end
+                
+    input.lbu = repmat(lbu,1,N);
+    input.ubu = repmat(ubu,1,N);
+    
+    input.lbx = repmat(lb_x,1,N);
+    input.ubx = repmat(ub_x,1,N);
+        
+    x = repmat(input.x0,1,N+1);  % initialize all shooting points with the same initial state
+    u = repmat(input.u0,1,N);    % initialize all controls with the same initial control
+    z = repmat(input.z0,1,N);    % initialize all algebraic state with the same initial condition
+    para = repmat(para0,1,N+1);  % initialize all parameters with the same initial para
+         
+    input.x=x;           % (nx by N+1)
+    input.u=u;           % (nu by N)
+    input.z=z;           % (nz by N)
+    input.od=para;       % (np by N+1)
+    input.W=Q;           % (ny by N)
+    input.WN=QN;         % (nyN by 1)
+     
+    input.lambda=zeros(nx,N+1);   % langrangian multiplier w.r.t. equality constraints
+    input.mu=zeros(N*nc+ncN,1);   % langrangian multipliers w.r.t. general inequality constraints
+    input.mu_u = zeros(N*nu,1);   % langrangian multipliers w.r.t. input bounds
+    input.mu_x = zeros(N*nbx,1);  % langrangian multipliers w.r.t. state bounds
+    
+end
---
+++ Simulator/matlab/MATMPC/models/KinematicVehicle.m
@@ -0,0 +1,102 @@
+%------------------------------------------%
+% Inverted Pendulum 
+  
+% "Kinematic and Dynamic Vehicle Models for Autonomous Driving
+% Control Design", Borrelli, 2015
+
+%------------------------------------------%
+
+
+%% Dimensions
+
+nx=3;  % No. of differential states
+nu=2;  % No. of controls
+nz=0;  % No. of algebraic states
+ny=3; % No. of outputs
+nyN=1; % No. of outputs at the terminal point
+np=3; % No. of model parameters
+nc=0; % No. of general constraints
+ncN=0; % No. of general constraints at the terminal point
+nbx = 0; % No. of bounds on states
+nbu = 2; % No. of bounds on controls
+
+% state and control bounds
+nbx_idx = 0; % indexs of states which are bounded
+nbu_idx = [1,2]; % indexs of controls which are bounded
+
+%% create variables
+
+import casadi.*
+
+states   = SX.sym('states',nx,1);   % differential states
+controls = SX.sym('controls',nu,1); % control input
+alg      = SX.sym('alg',nz,1);      % algebraic states
+params   = SX.sym('params',np,1);    % parameters
+refs     = SX.sym('refs',ny,1);     % references of the first N stages
+refN     = SX.sym('refs',nyN,1);    % reference of the last stage
+Q        = SX.sym('Q',ny,1);        % weighting matrix of the first N stages
+QN       = SX.sym('QN',nyN,1);      % weighting matrix of the last stage
+aux      = SX.sym('aux',ny,1);      % auxilary variable
+auxN     = SX.sym('auxN',nyN,1);    % auxilary variable
+
+%% Dynamics
+
+k = params(1);
+l_r = params(2);
+l_f = params(3);
+
+X = states(1);
+Y = states(2);
+psi = states(3);
+
+v = controls(1);
+d = controls(2);
+
+% sideslip
+beta = atan(l_r/(l_f+l_r)*tan(d));
+
+% eq
+dX = v*cos(psi+beta);
+dY = v*sin(psi+beta);
+dpsi = v/l_r*sin(beta);
+
+% spatial reformulation
+% s_dot = (1/(1-k*e_y))*(v*cos(e_psi));
+% e_psi_dot = dpsi-k*s_dot;
+% e_y_dot = v*sin(e_psi);
+
+
+
+% explicit ODE RHS
+x_dot=[ dX; ...
+        dY; ...
+        dpsi ... 
+        ];  
+ 
+% algebraic function
+z_fun = [];                   
+
+% implicit ODE: impl_f = 0
+xdot = SX.sym('xdot',nx,1);
+impl_f = xdot - x_dot;        
+     
+%% Objectives and constraints
+
+% inner objectives
+h = [psi;v;d];
+hN = h(1:nyN);
+
+% outer objectives
+obji = 0.5*(h-refs)'*diag(Q)*(h-refs);
+objN = 0.5*(hN-refN)'*diag(QN)*(hN-refN);
+
+obji_GGN = 0.5*(aux-refs)'*(aux-refs);
+objN_GGN = 0.5*(auxN-refN)'*(auxN-refN);
+
+% general inequality constraints
+general_con = [];
+general_con_N = [];
+
+%% NMPC discretizing time length [s]
+
+Ts_st = 0.05; % shooting interval time
