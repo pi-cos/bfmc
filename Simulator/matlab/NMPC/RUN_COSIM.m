a04a4ac9064817834035ca39faa35483ccb6afa7
Enrico Picotti <35801445+pi-cos@users.noreply.github.com>
2022-10-21 16:04:29 +0200

    first lap done! :)

--- Simulator/matlab/NMPC/RUN_COSIM.m
+++ Simulator/matlab/NMPC/RUN_COSIM.m
@@ -26,11 +26,12 @@ setup_solver
 %% cosim params
 
 setup.vel2motor = 1;
+setup.rad2deg = rad2deg(1);
 
 % !source /home/pico/Documents/GitHub/bfmc/Simulator/devel/setup.bash
 % !roslaunch sim_pkg map_with_car_nogui.launch
 % roslaunch sim_pkg map_with_car.launch
-% rosservice call /gazebo/reset_simulation
+% rosservice call /gazebo/reset_simulation --> NOT WORKING!!
 
 %%
 
